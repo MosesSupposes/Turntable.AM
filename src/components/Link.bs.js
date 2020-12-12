4478e07400f110d5fc4728709e927212874629e2
Moses A. Samuel <mosamuel98@gmail.com>
2020-12-11 17:44:25 -0800

    add children as props to the Link component

--- src/components/Link.bs.js
+++ src/components/Link.bs.js
@@ -6,13 +6,14 @@ import * as React from "react";
 function Link(Props) {
   var to_ = Props.to_;
   var setPage = Props.setPage;
+  var children = Props.children;
   return React.createElement("div", {
               onClick: (function (param) {
                   return Curry._1(setPage, (function (param) {
                                 return to_;
                               }));
                 })
-            });
+            }, children);
 }
 
 var make = Link;
--- src/components/Link.re
+++ src/components/Link.re
@@ -1,6 +1,6 @@
 open UsePage;
 
 [@react.component]
-let make = (~to_: Page.t, ~setPage) => {
-  <div onClick={_ => setPage(_ => to_)} />;
+let make = (~to_: Page.t, ~setPage, ~children) => {
+  <div onClick={_ => setPage(_ => to_)}> children </div>;
 };
