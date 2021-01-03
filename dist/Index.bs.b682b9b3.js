b94a2c5414e97ccc2c9d89b3d1c0821bc9cbe604
Moses A. Samuel <mosamuel98@gmail.com>
2021-01-02 22:01:21 -0800

    remove dead code

--- dist/Index.bs.b682b9b3.js
+++ dist/Index.bs.b682b9b3.js
@@ -41744,24 +41744,6 @@ var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
 var createEventHandlers = function createEventHandlers(player) {
   player.on("initialization_error", function (e) {
     console.error(e);
@@ -41807,86 +41789,7 @@ var startPlayer = function startPlayer(accessToken) {
   };
 };
 
-function SpotifyPlayer(accessToken) {
-  var _React$useState = _react.default.useState({
-    token: "",
-    deviceId: "",
-    error: "",
-    trackName: "",
-    artistName: "",
-    albumName: "",
-    playing: false,
-    position: 0,
-    duration: 0,
-    player: {}
-  }),
-      _React$useState2 = _slicedToArray(_React$useState, 2),
-      state = _React$useState2[0],
-      setState = _React$useState2[1];
-
-  var createEventHandlers = function createEventHandlers() {
-    state.player.on("initialization_error", function (e) {
-      return console.error(e);
-    }); // TODO: Set the page to the sign in page so the user can log back in.
-
-    state.player.on("authentication_error", function (e) {
-      return console.error(e);
-    });
-    state.player.on("account_error", function (e) {
-      return console.error(e);
-    });
-    state.player.on("playback_error", function (e) {
-      return console.error(e);
-    });
-    state.player.on("player_state_changed", function (state) {
-      return console.log(state);
-    });
-    state.player.on("ready", function (data) {
-      var device_id = data.device_id;
-      console.log("Let the music play on!");
-      setState(function (prevState) {
-        return _objectSpread(_objectSpread({}, prevState), {}, {
-          deviceId: device_id
-        });
-      });
-    });
-  };
-
-  var playerCheckInterval;
-
-  var checkForPlayer = function checkForPlayer() {
-    var accessToken = "BQDrvW_sGnbH4SHs_zMv5POZbwi7kX1ojjFVSTEKkOiZ4_Hna2CPo0nmL1tijfHGkky86h2iRidU45pJ3OBXUJfdss4zpVKncJbWC0gz0ARbGGvjphd4HHQd4EnK7z3tA8mH9lA565nxG0Xr0bpQh0Jt6pGM1UDdAWvXes_yZhgsM_xhIn4";
-
-    if (window.Spotify != null) {
-      clearInterval(playerCheckInterval);
-      setState(function (prevState) {
-        return _objectSpread(_objectSpread({}, prevState), {}, {
-          player: new window.Spotify.Player({
-            // TODO: Capture the user's name and use it here
-            name: "Moses' Spotify Player",
-            getOAuthToken: function getOAuthToken(cb) {
-              return cb(accessToken);
-            }
-          })
-        });
-      });
-      createEventHandlers();
-      state.player.connect();
-    }
-  }; // Start the Spotify Player once it's been mounted onto the DOM
-
-
-  _react.default.useEffect(function () {
-    playerCheckInterval = setInterval(function () {
-      return checkForPlayer();
-    }, 1000);
-  }, []);
-
-  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h2", null, "Now playing"), state.error && /*#__PURE__*/_react.default.createElement("p", null, "Error: ", state.error), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, "Artist: ", state.artistName), /*#__PURE__*/_react.default.createElement("p", null, "Track: ", state.trackName), /*#__PURE__*/_react.default.createElement("p", null, "Album: ", state.albumName)));
-}
-
-var _default = startPlayer; //export default SpotifyPlayer;
-
+var _default = startPlayer;
 exports.default = _default;
 },{"react":"../node_modules/react/index.js"}],"components/Home.bs.js":[function(require,module,exports) {
 "use strict";
